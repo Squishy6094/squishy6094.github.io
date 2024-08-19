9d5ef566fa2cd7f24584576e9499d3ae85f074c3
Squishy <74333752+Squishy6094@users.noreply.github.com>
2024-08-18 18:38:44 -0700

    fixed negative time

--- scripts/timeUpdate.js
+++ scripts/timeUpdate.js
@@ -11,8 +11,11 @@ setInterval(function() {
     let minutes = (d.getHours() + Math.floor(timezoneOffset / 60))*60 + d.getMinutes() + (timezoneOffset % 60)
     
     if (`${(minutes / 60 ^ 0)%12}` != '0') {timezone = (minutes / 60 ^ 0)%12} // Print Hour
+    if (timezone < 0){
+        timezone = timezone + 12
+    }
     if (d.getSeconds()%2 == 0 ) {timezone = timezone + ":"} else {timezone = timezone + "."} // Make Divider Blink
-    timezone = timezone + ("0" + (minutes % 60)).slice(-2); // Print Seconds
+    timezone = timezone + ("0" + (minutes % 60)).slice(-2); // Print Minutes
     if ((minutes / 60 ^ 0)%24 < 11) {timezone = timezone + " AM"} else {timezone = timezone + " PM"} // Append AM/PM based on hour
     
     // Update Text
