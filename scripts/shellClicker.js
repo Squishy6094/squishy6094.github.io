83977761759969687890d635b62b5e12816912f5
Squishy <74333752+Squishy6094@users.noreply.github.com>
2024-08-29 22:32:38 -0700

    Added a shell petting mechanic + blog post for it!

--- scripts/shellClicker.js
+++ scripts/shellClicker.js
@@ -1,6 +1,8 @@
-const owieAnimEnd = -200
+const owieAnimEnd = -70
 var owieAnim = owieAnimEnd
+var petAnim = 0
 var hitCount = 0
+var hasBeenHit = false
 var hitIntensity = 0
 
 // Preload Needed Assets - By Nanoo
@@ -16,6 +18,7 @@ function preloadImage(url) {
 
 preloadImage("images/shell-site/shell-clicker/shell-idle.gif");
 preloadImage("images/shell-site/shell-clicker/shell-ow.gif");
+preloadImage("images/shell-site/shell-clicker/shell-pet.gif");
 // preloadImage("images/shell-site/shell-clicker/shell-hurt-1.gif");
 // preloadImage("images/shell-site/shell-clicker/shell-hurt-2.gif");
 // preloadImage("images/shell-site/shell-clicker/shell-hurt-3.gif");
@@ -35,11 +38,67 @@ const hitAnims = [
     "shell-hurt-6.gif",
 ];
 
+// var shellPetStatus = 0
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        // Use event.pageX / event.pageY here
+        
+        const shellSize = document.getElementById("shellClickerLink").getBoundingClientRect();
+        if (event.pageY > shellSize.top && event.pageY > shellSize.bottom && hasBeenHit && owieAnim <= owieAnimEnd){
+            if (shellPetStatus == 0 && event.pageX > shellSize.left && event.pageX < shellSize.right) {
+                shellPetStatus = 1
+            }
+            if (shellPetStatus == 1 && (event.pageX < shellSize.left || event.pageX > shellSize.right)) {
+                shellPetStatus = 0
+                petAnim = 100
+                hitCount = hitCount - 1
+                if (hitCount >= 0) {
+                    navText.textContent = "Abuse: " + hitCount
+                } else {
+                    navText.textContent = "Headpats: " + hitCount*-1
+                }
+            }
+        } else {
+            shellPetStatus = 0
+        }
+        
+        console.log((event.pageX) + " | " + shellSize.left + " | " + event.pageY + " | " + shellPetStatus)
+    }
+})();
+
+const tickSpeed = 1000/30 // 30fps
 setInterval(function() {
     if (owieAnim > owieAnimEnd) {
+        petAnim = 0
         owieAnim = owieAnim - 1
         if (owieAnim < 0) {
             stupidBitch.src = 'images/shell-site/shell-clicker/shell-ow.gif'
+        }
+    } else if (petAnim > 0) {
+        petAnim = petAnim - 1
+        if (petAnim > 0) {
+            stupidBitch.src = 'images/shell-site/shell-clicker/shell-pet.gif'
         }
     } else {
         stupidBitch.src = 'images/shell-site/shell-clicker/shell-idle.png'
@@ -49,15 +108,23 @@ setInterval(function() {
         stupidBitch.style.left = (Math.random()*hitIntensity*2 - hitIntensity) + 'px';
         stupidBitch.style.bottom = (Math.random()*hitIntensity*2 - hitIntensity + 50) + 'px'; 
     }
-}, 10)
+}, tickSpeed)
 
 function shellOnClick() {
-    owieAnim = 50
+    owieAnim = 20
     hitIntensity = 10
+    if (hitCount < 0) {
+        hitCount = 0
+    }
     hitCount++
+    hasBeenHit = true
     navText.textContent = "Abuse: " + hitCount
     var anim = hitAnims[Math.floor(Math.random()*hitAnims.length)]
     stupidBitch.src = 'images/shell-site/shell-clicker/' + anim
+}
+
+function shellOnPet() {
+
 }
 
 document.getElementById("shellClickerLink").addEventListener("click", shellOnClick); 
